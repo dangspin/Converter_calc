6cbfeed2759e1cbce313b08bbdb4c8b67141cb67
dangspin <dangxiaoqian@gmail.com>
2020-05-20 17:53:22 +0800

    优化使用方式

--- logic.js
+++ logic.js
@@ -17,5 +17,30 @@ $(document).ready(function(){
     }
   })
 
+  $('.card.bg-danger.mb-2').on("click", () => {
+    $('.card.bg-danger.mb-2').replaceWith(input_template("fte"));
+  });
+
 
 });
+
+function card_template (name_of_card, color) {
+  return `<div class="card bg-${color} mb-2">
+    <div class="card-body">
+      <h4>${name_of_card.toUpperCase()}:</h4>
+      <div id="${name_of_card}out"></div>
+    </div>
+  </div>`
+}
+
+function input_template(name_of_card) {
+  return `<form>
+    <div class="form-group">
+      <input
+      id="timeInput"
+      type="number"
+      class="form-control form-control-lg"
+      placeholder="Enter ${name_of_card.toUpperCase()}s...">
+    </div>
+  </form>`
+}
